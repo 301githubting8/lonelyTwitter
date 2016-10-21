47c2baabfa7d2dc4db5ae88e3bb159476543a0c5
ting8 <ting8@ualberta.ca>
2016-10-20 18:14:23 -0600

    lab7

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
@@ -7,7 +7,13 @@ import com.searchly.jestdroid.DroidClientConfig;
 import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.searchbox.core.DocumentResult;
 import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
 
 /**
  * Created by romansky on 10/20/16.
@@ -20,12 +26,17 @@ public class ElasticsearchTweetController {
 
         @Override
         protected Void doInBackground(NormalTweet... tweets) {
+            verifySettings();
 
             for (NormalTweet tweet : tweets) {
                 Index index = new Index.Builder(tweet).index("testing").type("tweet").build();
 
                 try {
                     // where is the client?
+                    DocumentResult result = client.execute(index);
+                    if (result.isSucceeded()){
+                        tweet.setId(result.getId());
+                    }
                 }
                 catch (Exception e) {
                     Log.i("Error", "The application failed to build and send the tweets");
@@ -37,6 +48,37 @@ public class ElasticsearchTweetController {
     }
 
     // TODO we need a function which gets tweets from elastic search
+    public static class GetTweetsTask extends AsyncTask<String, Void, ArrayList<NormalTweet>>{
+        @Override
+        protected ArrayList<NormalTweet> doInBackground(String... search_parameters) {
+            verifySettings();
+
+            ArrayList<NormalTweet> tweets = new ArrayList<NormalTweet>();
+
+            //Assumption: Only the first search_paraemeter[0] is used.
+
+            Search search = new Search.Builder(search_parameters[0])
+                    .addIndex("testing")
+                    .addType("tweet")
+                    .build();
+            
+            try{
+                SearchResult result = client.execute(search);
+                if (result.isSucceeded()){
+                    //do something!
+                    List<NormalTweet> foundTweets = result.getSourceAsObjectList(NormalTweet.class);
+                    tweets.addAll(foundTweets);
+                }
+                else{
+                    Log.i("Error", "The search excured but it didn't work");
+                }
+            }
+            catch(Exception e){
+                Log.i("Error", "Executing the get tweets method failed");
+            }
+            return tweets;
+        }
+    }
 
     public static void verifySettings() {
         if (client == null) {
